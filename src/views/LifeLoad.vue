814ff3c24fde9afe76558fcd0c3ebc4489cd0abd
Alienware <1059171203@qq.com>
2025-05-16 21:58:09 +0800

    lifevue-src-key

--- src/views/LifeLoad.vue
+++ src/views/LifeLoad.vue
@@ -19,9 +19,9 @@
         navigation
         class="my-swiper"
       >
-        <SwiperSlide v-for="(src, idx) in slides" :key="idx">
+        <SwiperSlide v-for="(src, idx) in slides" :key="src.id">
           <img
-            :src="src"
+            :src="src.img"
             loading="lazy"
             class="slide-image"
             :alt="`工作经历图${idx + 1}`"
@@ -47,7 +47,7 @@ import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 
-const slides = [img1, img2, img3, img4];
+const slides = [{img:img1,id:'sassas121'}, {img:img2,id:'sassas111'}, {img:img3,id:'sassas321'}, {img:img4,id:'sassas421'}];
 const modules = [Navigation, Pagination, Autoplay, A11y];
 </script>
 
